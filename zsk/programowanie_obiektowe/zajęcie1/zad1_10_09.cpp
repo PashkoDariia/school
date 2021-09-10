34c584655b4adf3c99be0bebca7b8110fc9ec4f2
PashkoDariia <90031800+PashkoDariia@users.noreply.github.com>
2021-09-10 10:56:45 +0200

    Add files via upload

--- zsk/programowanie_obiektowe/zajęcie1/zad1_10_09.cpp
+++ zsk/programowanie_obiektowe/zajęcie1/zad1_10_09.cpp
@@ -7,26 +7,70 @@ class Worker{
 //deklaracja zmiennych czloncowskich (pol, wlasciwosci)
       string name;	
       string surname;
-      int age; //dokoñczyc, sprawdziæ typ danych
+      unsigned short int yearBirthday;
+      string nationality;
+      float height;
+      char gender;
+      //unsigned short int age; //dokoñczyc, sprawdziæ typ danych
+      
 //definicja funkcji czlonkowszkiej (metody)
+      void showName(){
+	  	cout << "\nImiê pracownika: " <<name;
+	  };
       void showSurname(){
       	cout<<"\nNazwisko pracownika: " <<surname;
 	  };
+	  void showYearBirthday(){
+	  	cout<<"\nRok urodzenia: "<<yearBirthday;
+	  }
+	  void showNationality(){
+	  	cout << "\nNarodowoœæ: " <<nationality;
+	  };
+	  void showHeight(){
+	  	cout << "\nWzrost: "<<height;
+	  };
+	  void showGender(){
+	  	cout << "P³eæ: "<<gender;
+	  }
+	  
 //deklaracja (prototyp) metody
       void showAllData();
 };
+
 //definicja metody showAllData
       void Worker::showAllData(){
       	cout << "\nImiê i nazwisko: "<< name << " " << surname;
+      	cout << "\nNarodowoœæ: " << nationality;
+      	cout << "\nWzrost: "<<height<< " cm";
+      	cout<<"\nRok urodzenia: "<<yearBirthday;
+      	cout << "\nP³eæ: ";
+      	 
+      	
+      	switch(gender){
+      		case 'm':
+      			cout<< "mê¿czyzna";
+      			break;
+      			case 'w':
+      				cout<< "kobieta";
+      				break;
+      				default:
+      					cout<<"-";
+		  }
 	  };
+	  
 	  
 int main(int argc, char** argv) {
 	setlocale(LC_CTYPE,"polish");
 	Worker pracownik1;
 	pracownik1.name="Krystian";
 	pracownik1.surname="Nowak";
-	cout << pracownik1.name;
-	pracownik1.showSurname();
+    pracownik1.nationality  ="Polish";
+    pracownik1.height=181.5;
+    pracownik1.gender='m';
+    pracownik1.yearBirthday =2005;
+	//cout << pracownik1.name;
+	//pracownik1.showSurname();
 	pracownik1.showAllData();
 	return 0;
 }
+
