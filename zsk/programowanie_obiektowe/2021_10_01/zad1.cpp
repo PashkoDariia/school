0dba51d3b1500a6b48da1a4b0eb30a690f881de8
PashkoDariia <90031800+PashkoDariia@users.noreply.github.com>
2021-10-01 11:21:25 +0200

    Add files via upload

---
+++ zsk/programowanie_obiektowe/2021_10_01/zad1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Time{
+	public:
+		static string s_time;
+		static string s_date;
+		 
+};
+
+string Time::s_date="Fri Oct 01 2021";
+string Time::s_time=""
+int main(int argc, char** argv) {
+	return 0;
+}
